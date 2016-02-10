71853da6d000f7c1a335fb70b0c81b33b618073b
iAmNathanJ <jacobs@heysparkbox.com>
2016-02-10 14:17:42 -0500

    update build

--- dist/index.js
+++ dist/index.js
@@ -4,11 +4,45 @@ function _interopDefault (ex) { return (ex && (typeof ex === 'object') && 'defau
 
 var express = _interopDefault(require('express'));
 
+function life() {
+
+  var _Rand = Math.random;
+  var _Round = Math.round;
+
+  var mappable = function mappable(len) {
+    var arr = [];
+    for (var i = 0; i < len; i++) {
+      arr.push(0);
+    }
+    return arr;
+  };
+
+  var populate = function populate(acc, cur) {
+    return acc + String(_Round(_Rand()));
+  };
+
+  return {
+
+    state: [],
+
+    seed: function seed(rows, cols) {
+      this.state = mappable(rows).map(function (row) {
+        return mappable(cols).reduce(populate, '');
+      });
+    },
+    generate: function generate() {
+      return this.state.map(function (row) {});
+    }
+  };
+};
+
 var app = express();
 var port = 3000;
 
 app.get('/', function (req, res) {
-  res.json({ stuff: [] });
+  var life$$ = life();
+  life$$.seed(20, 10);
+  res.json(life$$.state);
 });
 
 app.listen(port, function () {
