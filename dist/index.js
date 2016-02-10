8854c4cf6428cd75772ed1637853dba382956cb8
iAmNathanJ <jacobs@heysparkbox.com>
2016-02-10 09:46:48 -0500

    add babel plugin to rollup

--- dist/index.js
+++ dist/index.js
@@ -1,18 +1,16 @@
 'use strict';
 
-function _interopDefault(ex) {
-  return ex && typeof ex === 'object' && 'default' in ex ? ex['default'] : ex;
-}
+function _interopDefault (ex) { return (ex && (typeof ex === 'object') && 'default' in ex) ? ex['default'] : ex; }
 
 var express = _interopDefault(require('express'));
 
-const app = express();
-const port = 3000;
+var app = express();
+var port = 3000;
 
-app.get('/', (req, res) => {
+app.get('/', function (req, res) {
   res.json({ stuff: [] });
 });
 
-app.listen(port, () => {
-  console.log(`Listening on http://localhost:${ port }`);
+app.listen(port, function () {
+  console.log('Listening on http://localhost:' + port);
 });
