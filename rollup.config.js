1c0e3d0863ae70616434027e299b76c7f007c14d
iAmNathanJ <jacobs@heysparkbox.com>
2016-02-10 09:48:57 -0500

    remove dev dep: rollup-plugin-json

--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,8 @@
-import json from 'rollup-plugin-json';
 import babel from 'rollup-plugin-babel';
 
 export default {
   entry: 'dev/index.js',
   format: 'cjs',
-  plugins: [ json(), babel() ],
+  plugins: [ babel() ],
   dest: 'dist/index.js'
 };
