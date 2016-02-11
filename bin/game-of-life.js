fde5deca96492b933e41dd2f322e534e2782f96d
iAmNathanJ <jacobs@heysparkbox.com>
2016-02-11 09:44:34 -0500

    add use strict

--- bin/game-of-life.js
+++ bin/game-of-life.js
@@ -1,21 +1,24 @@
+'use strict';
+
+const _Rand = Math.random;
+const _Round = Math.round;
+
+const mappable = (len) => {
+  let arr = [];
+  for(let i = 0; i < len; i++) {
+    arr.push(0);
+  }
+  return arr;
+};
+
+const populate = (acc) => acc + String(_Round(_Rand()));
+const getAdjacent = (row, col, state) => {
+};
+
+
 module.exports = function life() {
 
   // Array.prototype.grab = (index, def) => this[index] || def;
-
-  const _Rand = Math.random;
-  const _Round = Math.round;
-
-  const mappable = (len) => {
-    let arr = [];
-    for(let i = 0; i < len; i++) {
-      arr.push(0);
-    }
-    return arr;
-  };
-
-  const populate = (acc) => acc + String(_Round(_Rand()));
-  const getAdjacent = (row, col, state) => {
-  };
 
   return {
 
