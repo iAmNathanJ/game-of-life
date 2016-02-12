b666624163b70dd2d4f4a81ea8823f6366b814e2
iAmNathanJ <jacobs@heysparkbox.com>
2016-02-12 11:36:44 -0500

    update index.js: fix CORS setting

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const port = 3000;
 
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Origin', 'http://127.0.0.1:8080');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
 });
