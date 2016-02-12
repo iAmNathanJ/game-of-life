2163d9446e6d211f9a4411a12ce4174267d36134
iAmNathanJ <jacobs@heysparkbox.com>
2016-02-12 16:06:47 -0500

    updates: i think it works

--- consumers/browser/main.js
+++ consumers/browser/main.js
@@ -2,13 +2,45 @@
 
   'use strict';
 
-  let xhr = new XMLHttpRequest();
+  const DOM = {
+    btnGenerate: document.querySelector('.btn'),
+    main: document.querySelector('.app')
+  };
+
+  let xhr = new XMLHttpRequest()
+    , currentState;
+
+  function getNextGen(state) {
+    xhr.open('POST', 'http://localhost:3000/api');
+    xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xhr.send(`json=${state}`);
+  }
+
+  function updateDom(state) {
+    let div = document.createElement('div')
+      , text = '';
+
+    DOM.main.innerHTML = '';
+    JSON.parse(state).forEach((row) => {
+      text += `${row}<br />`;
+    });
+    div.innerHTML = text;
+    DOM.main.appendChild(div);
+  }
 
   xhr.addEventListener('load', (e) => {
-    console.log(e);
+    currentState = e.target.responseText;
+    updateDom(currentState);
   });
 
-  xhr.open('GET', 'http://localhost:3000/api');
-  xhr.send();
+  DOM.btnGenerate.addEventListener('click', (e) => {
+    getNextGen(currentState);
+  });
+
+
+  (function create() {
+    xhr.open('GET', 'http://localhost:3000/api');
+    xhr.send();
+  })();
 
 })();
