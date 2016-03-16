89aaa3b75c8761fa641e09d89bf150ca6753f8c1
iAmNathanJ <jacobs@heysparkbox.com>
2016-03-16 09:34:50 -0400

    simple test added

--- test/test.js
+++ test/test.js
@@ -3,7 +3,28 @@
 const test = require('tape');
 const life = require('../lib/game-of-life.js');
 
-test('Life is a thing.', t => {
+test('life is a thing', t => {
   t.ok(life, 'it lives');
+  t.end();
+});
+
+test('life generates life', t => {
+
+  life.seed([
+    { x: 0, y: 0 },
+    { x: 1, y: 0 },
+    { x: 0, y: 1 }
+  ]);
+
+  life.generate();
+
+  let expected = [
+    { x: 0, y: 0 },
+    { x: 1, y: 0 },
+    { x: 0, y: 1 },
+    { x: 1, y: 1 }
+  ];
+
+  t.deepEqual(life.state, expected, 'it generates');
   t.end();
 });
