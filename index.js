6b4b49b2bc393e63826ce5c26ff45d64d02f62f4
iAmNathanJ <jacobs@heysparkbox.com>
2016-03-16 13:13:22 -0400

    cleanup

--- index.js
+++ index.js
@@ -1,12 +1,3 @@
 'use strict';
 
 const life = require('./lib/game-of-life');
-
-life.seed([
-  { x: 0, y: 0 },
-  { x: 1, y: 0 },
-  { x: 0, y: 1 }
-]);
-
-life.generate();
-console.log(life.state);
